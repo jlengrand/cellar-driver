23c4da98cc0b5e8f02004d5e259d7bc6c72371a3
Julien Lengrand-Lambert <julien@lengrand.fr>
2020-07-16 20:23:01 +0200

    Getting somewhere?

--- src/main/c/compile_arm.sh
+++ src/main/c/compile_arm.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-rm testAdafruit;
 arm-linux-gnueabihf-gcc -Wall -I. -Iadafruit/Raspberry_Pi_2 adafruit/common_dht_read.c adafruit/Raspberry_Pi_2/pi_2_mmio.c adafruit/Raspberry_Pi_2/pi_2_dht_read.c testAdafruit.c -o testAdafruit
